1b94c0cbc92a56f9db34da709009da88a4da5b4e
leslie <leslieejeh@cmail.com>
2022-09-19 16:02:45 -0400

    i have added a main method to test the code

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -4,17 +4,23 @@ public class AddressBook {
     private ArrayList<BuddyInfo> buds;
 
 
-    public AddressBook(BuddyInfo buddy) {
+    public AddressBook() {
        this.buds=new ArrayList<BuddyInfo>();
        System.out.println("Address book");
     }
 
     public void addBuddy(BuddyInfo friend){
-
+        buds.add(friend);
     }
     public void removeBuddy(BuddyInfo friend) {
+        buds.remove(friend);
 
-
+    }
+    public static void main(String[] args){
+        BuddyInfo  baby= new BuddyInfo(675, "randa","1125 colonel by dr");
+        AddressBook addressBook=new AddressBook();
+        addressBook.addBuddy(baby);
+        addressBook.removeBuddy(baby);
     }
 
 
