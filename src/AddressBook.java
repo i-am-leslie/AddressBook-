905202a677610d90b26bd881a351fa488fef60fb
leslie <leslieejeh@cmail.com>
2022-09-19 15:41:54 -0400

    begining of address book

---
+++ src/AddressBook.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.*;
+public class AddressBook {
+    private ArrayList<BuddyInfo> buds;
+
+
+    public AddressBook(BuddyInfo buddy) {
+       this.buds=new ArrayList<BuddyInfo>();
+       System.out.println("Address book");
+    }
+
+    public void addBuddy(BuddyInfo friend){
+
+    }
+    public void removeBuddy(BuddyInfo friend) {
+
+
+    }
+
+
+}
